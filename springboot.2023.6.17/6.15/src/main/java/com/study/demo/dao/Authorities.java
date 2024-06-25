46b31d5e1f39f0dad6869046e6d0f89ff427c017
liufu <131677124+liufu668@users.noreply.github.com>
2024-06-25 18:03:05 +0800

    Delete springboot.2023.6.17 directory

--- springboot.2023.6.17/6.15/src/main/java/com/study/demo/dao/Authorities.java
+++
@@ -1,7 +0,0 @@
-package com.study.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface Authorities extends JpaRepository<com.study.demo.entity.Authorities,Integer>, JpaSpecificationExecutor<com.study.demo.entity.Authorities> {
-}
